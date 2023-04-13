ec1ca3195b9183a3ca52983615a2ee1a33f15a9d
DOROTHY2322 <kimelidorothy@gmail.com>
2023-04-13 20:13:13 +0300

    Updating Admins controllers

--- api/app/controllers/admins_controller.rb
+++ api/app/controllers/admins_controller.rb
@@ -19,77 +19,77 @@ class AdminsController < ApplicationController
     end
   end
   
-  # Add a new book
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      render json: @book, status: :created
-    else
-      render json: { error: @book.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+  # # Add a new book
+  # def create
+  #   @book = Book.new(book_params)
+  #   if @book.save
+  #     render json: @book, status: :created
+  #   else
+  #     render json: { error: @book.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
 
-  # Update a book from the store
-  def update
-    if @book.update(book_params)
-      render json: @book, status: :ok
-    else
-      render json: { error: @book.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+  # # Update a book from the store
+  # def update
+  #   if @book.update(book_params)
+  #     render json: @book, status: :ok
+  #   else
+  #     render json: { error: @book.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
 
-  # Delete from the store
-  def destroy
-    @book.destroy
-    head :no_content
-  end
+  # # Delete from the store
+  # def destroy
+  #   @book.destroy
+  #   head :no_content
+  # end
 
-  # Approve all orders
-  def approve_all_orders
-    Order.update_all(approved: true)
-    head :no_content
-  end
+  # # Approve all orders
+  # def approve_all_orders
+  #   Order.update_all(approved: true)
+  #   head :no_content
+  # end
 
-  # Reject all orders
-  def reject_all_orders
-    Order.update_all(approved: false)
-    head :no_content
-  end
+  # # Reject all orders
+  # def reject_all_orders
+  #   Order.update_all(approved: false)
+  #   head :no_content
+  # end
 
-  # Route: PUT /lendings/approve_all
-  # Approve all lendings
-  def approve_all_lendings
-    Lending.update_all(approved: true)
-    head :no_content
-  end
+  # # Route: PUT /lendings/approve_all
+  # # Approve all lendings
+  # def approve_all_lendings
+  #   Lending.update_all(approved: true)
+  #   head :no_content
+  # end
 
-  # Route: PUT /lendings/reject_all
-  # Reject all lendings
-  def reject_all_lendings
-    Lending.update_all(approved: false)
-    head :no_content
-  end
+  # # Route: PUT /lendings/reject_all
+  # # Reject all lendings
+  # def reject_all_lendings
+  #   Lending.update_all(approved: false)
+  #   head :no_content
+  # end
 
-  # Route: GET /books
-  # Get all books in store
-  def index
-    @books = Book.all
-    render json: @books, status: :ok
-  end
+  # # Route: GET /books
+  # # Get all books in store
+  # def index
+  #   @books = Book.all
+  #   render json: @books, status: :ok
+  # end
 
-  # Route: GET /library/books
-  # Get all books in library
-  def library_books
-    @books = Book.where(in_library: true)
-    render json: @books, status: :ok
-  end
+  # # Route: GET /library/books
+  # # Get all books in library
+  # def library_books
+  #   @books = Book.where(in_library: true)
+  #   render json: @books, status: :ok
+  # end
 
   private
 
-  # Set permitted admin parameters
-  def admin_params
-    params.permit(:username, :email, :password)
-  end
+  # # Set permitted admin parameters
+  # def admin_params
+  #   params.permit(:username, :email, :password)
+  # end
 
   # Set permitted book parameters
   def book_params
@@ -97,8 +97,8 @@ class AdminsController < ApplicationController
   end
 
   # Find book by ID
-  def set_book
-    @book = Book.find(params[:id])
-  end
+  # def set_book
+  #   @book = Book.find(params[:id])
+  # end
 
 end
