854b95a57ab7cf420aa9cf8cb16397714722f873
feisalbireh <feisal.bireh@student.moringaschool.com>
2023-04-22 06:45:28 +0300

    added previous lendings

--- client/src/components/account.jsx
+++ client/src/components/account.jsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './account.css'
 
-
 function AccountPage() {
   console.log('AccountPage component rendered');
-  const [requests, setRequests] = useState([
+  
+  const [purchases, setPurchases] = useState([
     { id: 1, date: '20-04-2023', status: 'completed', orders: ['Book A', 'Book B'] },
     { id: 2, date: '10-02-2023', status: 'in progress', orders: ['Book C', 'Book D'] },
     { id: 3, date: '07-03-2023', status: 'pending', orders: ['Book E', 'Book F'] },
+  ]);
+  
+  const [lendings, setLendings] = useState([
+    { id: 1, date: '20-04-2023', status: 'returned', book: 'Book G' },
+    { id: 2, date: '10-02-2023', status: 'borrowed', book: 'Book H' },
+    { id: 3, date: '07-03-2023', status: 'overdue', book: 'Book I' },
   ]);
 
   const [showReturnForm, setShowReturnForm] = useState(false);
@@ -26,20 +32,34 @@ function AccountPage() {
   return (
     <div>
       <h1>My Account</h1>
+      
       <h2>Previous Purchases</h2>
       <ul>
-        {requests.map((request) => (
-          <li key={request.id}>
-            <p>Date: {request.date}</p>
-            <p>Status: {request.status}</p>
-            <p>Orders: {request.orders.join(', ')}</p>
+        {purchases.map((purchase) => (
+          <li key={purchase.id}>
+            <p>Date: {purchase.date}</p>
+            <p>Status: {purchase.status}</p>
+            <p>Orders: {purchase.orders.join(', ')}</p>
           </li>
         ))}
       </ul>
+      
+      <h2>Previous Lendings</h2>
+      <ul>
+        {lendings.map((lending) => (
+          <li key={lending.id}>
+            <p>Date: {lending.date}</p>
+            <p>Status: {lending.status}</p>
+            <p>Book: {lending.book}</p>
+          </li>
+        ))}
+      </ul>
+      
       <button onClick={handleReturnRequest}>Initiate Return Request</button>
       <Link to="/payment">
         <button>Pay for an Order</button>
       </Link>
+      
       {showReturnForm && (
         <form onSubmit={handleFormSubmit}>
           <label>
