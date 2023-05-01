f53a6c51a430f4e1648416b6dbcf27f9ab79daa3
DOROTHY2322 <kimelidorothy@gmail.com>
2023-05-01 12:26:23 +0300

    STYLING

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,17 +1,16 @@
-
-
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import "./Login.css"
+import "./Login.css";
+
 export default function Login() {
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [error, setError] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoggingIn, setIsLoggingIn] = useState(false);
-  const navigate = useNavigate()
-  // const history = useHistory();
+  const navigate = useNavigate();
+
   const handleLogin = (e) => {
     e.preventDefault();
     setIsLoggingIn(true);
@@ -25,70 +24,66 @@ export default function Login() {
         password,
       }),
     })
-    .then(res=>{
-      if (res.ok) {
-        navigate('/home');
-        setIsLoggedIn(true);
-        // setMessage(res.json());
-      }
-      return res.json();
-    })
-    .then(data=> {
-      localStorage.setItem('user', `${data.roles.map(e=>e.name).toString()}|${data.id.toString()}|${data.username.toString()}`);      
-      console.log(data);
-    })
-    .catch(error=>{
-      setMessage("");
-      setError(error);
-      setIsLoggingIn(false);
-    })
+      .then((res) => {
+        if (res.ok) {
+          navigate("/home");
+          setIsLoggedIn(true);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        localStorage.setItem(
+          "user",
+          `${data.roles.map((e) => e.name).toString()}|${data.id.toString()}|${data.username.toString()}`
+        );
+      })
+      .catch((error) => {
+        setMessage("");
+        setError(error);
+        setIsLoggingIn(false);
+      });
   };
+
   return (
-      <div className="auth-form-container">
-        <h2>Login</h2>
-     
-        <form className="login-forrm" onSubmit={handleLogin}>
-        
-            <label htmlFor="usernameOrEmail">Username</label>
-                
-                <input
-                  type="text"
-                  placeholder="ENTER YOUR USERNAME OR EMAIL"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                placeholder="ENTER PASSWORD"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            
-              
-                <button type="submit" disabled={isLoggingIn}>
-                  {isLoggingIn ? "LoggingIn..." : "LOGIN"}
-                </button>
-                
-                <div>
-                  {message && <p>{message}</p>}
-                  {error && <p>{error}</p>}
-                </div>
-                
-                <p>
-                  Not a member?{" "}
-                  <NavLink
-  to="/register"
-  className="link-to-register"
->
-  Register
-</NavLink>
-                </p>
-            
-           
-        </form>
-      </div>
- 
-  
+    <div className="auth-form-container">
+  <form className="auth-form" onSubmit={handleLogin}>
+    <h2 className="auth-form-heading">Login</h2>
+    <div className="auth-form-field">
+      <label htmlFor="usernameOrEmail" className="auth-form-label">Username</label>
+      <input
+        type="text"
+        placeholder="ENTER YOUR USERNAME OR EMAIL"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="auth-form-input"
+      />
+    </div>
+    <div className="auth-form-field">
+      <label htmlFor="password" className="auth-form-label">Password</label>
+      <input
+        type="password"
+        placeholder="ENTER PASSWORD"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="auth-form-input"
+      />
+    </div>
+    <button type="submit" className="auth-form-button" disabled={isLoggingIn}>
+      {isLoggingIn ? "Logging In..." : "LOGIN"}
+    </button>
+    <div>
+      {message && <p className="auth-form-text">{message}</p>}
+      {error && <p className="auth-form-error">{error}</p>}
+    </div>
+    <p className="auth-form-text">
+      Not a member?{" "}
+      <NavLink to="/register" className="link-to-register">
+        Register
+      </NavLink>
+    </p>
+  </form>
+</div>
+
   );
 }
+
